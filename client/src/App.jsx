1fbade169b053e0d08fd29413a880653d1870eca
G <g@Gs-MacBook-Pro.fios-router.home>
2019-06-15 09:36:54 -0400

    Semantic react setup.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import './css/main.css';
+import { Icon } from 'semantic-ui-react';
 
 class App extends Component {
     render() {
         return (
             <div id="App">
-                <p>React App Template</p>
+                <p>React App</p>
+                <p>Semantic React Showing Spinner</p>
+                <Icon size='massive' loading name='certificate' />
             </div>
         );
     }
