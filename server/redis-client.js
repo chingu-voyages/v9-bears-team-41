869fe3c850d29887bd820956c21c3cf7f472aaaa
Ji <g@Gs-MacBook-Pro.fios-router.home>
2019-06-23 19:46:08 -0400

    Code styling.

--- server/redis-client.js
+++ server/redis-client.js
@@ -5,12 +5,12 @@ const redisHost = process.env.REDIS_HOST;
 
 function getRedisClient() {
     const client = redis.createClient({ host: redisHost });
+
     client.on('connect', () => console.log('Redis client connected!'));
     client.on('ready', () => console.log('Redis client ready!'));
-    client.on('error', error => {
-        console.error('Error:', error);
-    });
+    client.on('error', error => console.error('Error:', error));
     client.on('end', () => console.log('Redis client connection closed!'));
+
     client.getAsync = promisify(client.get);
     return client;
 }
