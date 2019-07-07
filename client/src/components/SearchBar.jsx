2412762a7f75bcd0f86e8bed64c25b50033426c9
G <g@Gs-MacBook-Pro.fios-router.home>
2019-07-07 16:54:33 -0400

    Search bar.

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
+import './SearchBar.css';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
+        this.inputHandler = this.inputHandler.bind(this);
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+        this.state = {
+            inputValue: ''
+        };
     }
 
     async componentDidMount() {
         const searchUrl = 'localhost:3001/file/search';
     }
 
+    inputHandler(e) {
+        this.setState({ inputValue: e.target.value });
+    }
+
+    buttonClickHandler() {
+        const { inputValue } = this.state;
+        this.props.onSearchUpdated(inputValue);
+    }
+
     render() {
         return (
-            <div className="SearchBar">
-                search bar
+            <div id='searchBar'>
+                <div id='searchBarInput'>
+                    <input
+                        placeholder='Enter search terms'
+                        type='text'
+                        value={this.state.inputValue}
+                        onChange={this.inputHandler}
+                    />
+                </div>
+                <div id='searchBarButton'>
+                    <button onClick={this.buttonClickHandler}>
+                        SEARCH
+                    </button>
+                </div>
             </div>
         );
     }
 }
+
+SearchBar.propTypes = {
+    onSearchUpdated: PropTypes.func.isRequired
+};
 
 export default SearchBar;
